0cde108e315416ebef8b71df0349ecf036d74a6a
Faiyaz286 <75643370+Faiyaz286@users.noreply.github.com>
2021-04-05 21:01:19 +0600

    Add files via upload

---
+++ Task5/emptyValidation.js
@@ -0,0 +1,8 @@
+function emptyValidation() {
+  var fname = document.getElementById("firstname").value;
+  var email = document.getElementById("email").value;
+  if (fname == "" || email == "") {
+    alert("Name must be filled out")
+    return false;
+  }
+}
